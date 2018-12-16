82fa91330ebdf4584e7671989e2154254b1bcca2
masterUNG <phrombutr@gmail.com>
2018-12-16 09:40:37 +0700

    Variable and Constant

---
+++ BasicSwift.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//Varable and Constant
+
+//Implicite การประกาศตัวแปรโดยใช้ Value เป็นตัวกำหนด Data Type
+// การประกาศแบบ Var คือ สามารถแก้ไขค่าได้
+var nameString = "MasterUNG"
+nameString = "Doramon"
+

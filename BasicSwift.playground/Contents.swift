5f7b313a37c8e75a7e874255ee90563d7fd36395
masterUNG <phrombutr@gmail.com>
2018-12-16 09:54:27 +0700

    Operate Success

--- BasicSwift.playground/Contents.swift
+++ BasicSwift.playground/Contents.swift
@@ -7,3 +7,29 @@ import UIKit
 var nameString = "MasterUNG"
 nameString = "Doramon"
 
+
+let surnameString = "Phrombutr"
+//surnameString = "Phrimbutr123" ไม่สามารถแก้ไขค่าได้ เพราะ เป็น let
+
+//Explicit คือการประกาศตัวแปร ที่กำหนด Data Type ไปเลย
+var name1: String = "Nopita"    // for String
+var number1: Int = 123          // for Integer
+var number2: Double = 123.45    // For Double
+var status: Bool = true         // Boolene
+
+//Operating
+
+var answer1: String = name1 + nameString
+answer1 = answer1 + String(number1) // Any to String
+var answer2: Int = number1 + 100
+answer2 = answer2 + Int(number2)
+
+
+
+
+
+
+
+
+
+
